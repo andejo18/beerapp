9da94fce4540567b4d680c95a8289fad0bea2c50
Joel Anderson <andejo18@luther.edu>
2013-05-10 09:51:58 -0500

    put in a comment in the controller for beerprofile with url

--- controllers/main.py
+++ controllers/main.py
@@ -1,17 +1,24 @@
-from brewerydb import *
+# from brewerydb import *
+# BreweryDb.configure("72166c358bc617ab5c18ecf67630a322",["http://api.brewerydb.com/v2/"])
 
+
+
+# use this as an example:
+# http://api.brewerydb.com/v2/search/?key=72166c358bc617ab5c18ecf67630a322&q=guinness&type=beer
 
 def beerprofile():
 
     # form=FORM('Brewery Database url:', INPUT(_name='url'), INPUT(_type='submit'))
     # return dict(form=form)
 
+    # print BreweryDb.beers()
+
     return dict()
 
 def echo():
 
 
-    
+
     return request.vars.name
 
 
